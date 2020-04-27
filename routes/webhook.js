ea73776b72b648e88e97e2c779a6503fd33d89ec
yusei-hyodo <yusei.hyodo@gmail.com>
2020-04-27 23:05:01 +0900

    メイン関数の完成

--- routes/webhook.js
+++ routes/webhook.js
@@ -41,34 +41,72 @@ router.post('/', line.middleware(config), async (req, res) => {
 
 /**
  * メイン関数
- *
  */
 const handlerEvent = async (event) => {
   // Webhookの検証
   if (event.replyToken && event.replyToken.match(/^(.)\1*$/)) {
-    return "Webhookの検証"
+    return 'Webhookの検証'
   }
 
   const replyToken = event.replyToken
 
+  // イベントの処理
   switch (event.type) {
+    case 'follow':
+      return 'フォローされました'
+
+    case 'unfollow':
+      return 'フォロー解除されました'
+
     case 'message':
-      const message = event.message;
+      const message = event.message
+      const messageId = event.message.id
       switch (message.type) {
-        case "text":
-          await replyText(replyToken, message.text)
-          return "オウム返し成功"
+        case 'image':
+          // 画像を受信した際の処理
+          const text = await imageToText(messageId)
+          await replyText(replyToken, text)
+          return '画像を文字起こししました'
+
+        case 'audio':
+          // 音声を受信した際の処理
+          const text = await audioToText(messageId)
+          await replyText(replyToken, text)
+          return '音声を文字起こししました'
+
+        case 'video':
+          // 動画を受信した際の処理
+          const text = await videoToText(messageId)
+          await replyText(replyToken, text)
+          return '動画を文字起こししました'
+
+        default:
+          const text = '画像、音声、動画のどれかを送信してください'
+          await replyText(replyToken, text)
+          return '画像、音声、動画以外を受信'
       }
+    
+    default:
+      return 'その他'
   }
-  
-  await replyText(replyToken, "テキストを送信してください")
-  return "オウム返し失敗"
 }
 
 /**
  * テキストを返信する関数
  * @param {String} token
  * @param {String[] | String} texts
+ */
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+};
+
+/**
+ * 画像をテキストに変換する関数
+ * @param {Number} messageId
  */
 const replyText = (token, texts) => {
   texts = Array.isArray(texts) ? texts : [texts];
